357635d9e518f9a2a08adb89c1deacf95fac502c
KeiichiNaritomi <104675165+naritomi55@users.noreply.github.com>
2022-05-07 12:50:16 +0000

    index.html.erb編集中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,26 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/books/show'
   end
 
   def index
+   @book = Book.all
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+  def book_params
+    params.permit(:title, :body)
   end
 end
