1ddf2ae6afb825a230d5a32e9083be5c35e068fc
KeiichiNaritomi <104675165+naritomi55@users.noreply.github.com>
2022-05-09 13:19:20 +0000

    [3-11]フラッシュメッセージまで実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,19 +5,36 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to '/books/show'
+    if book.save
+      redirect_to book, flash: { notice: 'Posting successfully'}
+    else
+      redirect_to book_path(book.id)
+    end
   end
 
   def index
-   @book = Book.all
+   @books = Book.all
   end
 
   def show
-  @book = Book.find(params[:id])
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id), notice: "Updated successfully"
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
 
   private
